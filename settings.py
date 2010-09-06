fb3b4427fa75686766a62a32a095586cce95e1d5
strider <strider@may-laptop>
2010-09-07 00:18:57 +0200

    added fixtures

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 
 # Django settings for lutrisweb project.
 import os
+import socket
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -16,7 +17,12 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'lutris'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'admin'         # Not used with sqlite3.
+if socket.gethostname() == 'astoria':
+    DATABASE_PASSWORD = 'admin'         # Not used with sqlite3.
+elif socket.gethostname() == 'may-laptop':
+    DATABASE_PASSWORD = 'mysql'
+else:
+    DATABASE_PASSWORD = 'admin'
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
