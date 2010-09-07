3d2718e78a4583af249b7ae0e3a39e54fc3e8db5
Mathieu Comandon <mathieu.comandon@free.fr>
2010-09-07 18:25:04 +0200

    small modifications to the model

--- games/models.py
+++ games/models.py
@@ -6,28 +6,42 @@ import os
 def generate_installer_name(instance, filename):
     return os.path.join('installers', instance.slug + ".yml")
 
+class Platform(models.Model):
+    name = models.CharField(_('Name'), max_length=127)
+    slug = models.SlugField(unique=True)
+    icon = models.ImageField(upload_to='platform_icons', blank=True)
+
+    def __unicode__(self):
+        return self.name
+
 class Runner(models.Model):
-    name = models.CharField(_("Name"), max_length = 200)
-    website = models.CharField(_("Website"), max_length = 200)
+    name = models.CharField(_("Name"), max_length=127)
+    slug = models.SlugField(unique=True)
+    website = models.CharField(_("Website"), max_length=127)
+    icon = models.ImageField(upload_to='runner_icons', blank=True)
 
     def __unicode__(self):
         return self.name
 
 class Genre(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.name
 
 class Game(models.Model):
-    name = models.CharField(max_length = 200)
-    slug = models.SlugField(unique = True)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     runner = models.ForeignKey(Runner)
-    publisher = models.CharField(max_length = 200)
-    developer = models.CharField(max_length = 200)
-    website = models.CharField(max_length = 200)
-    installer = models.FileField(upload_to = generate_installer_name, blank = True, null = True)
+    publisher = models.CharField(max_length=200)
+    developer = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    installer = models.FileField(upload_to=generate_installer_name, blank=True, null=True)
     genre = models.ForeignKey(Genre)
+    icon = models.ImageField(upload_to='game_icons', blank=True)
+    cover = models.ImageField(upload_to='game_covers', blank=True)
+
+
 
     def __unicode__(self):
         return self.name
