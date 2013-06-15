6dc786ad73d20da987079dc12b28b63fbef762e4
Mathieu Comandon <strycore@gmail.com>
2013-06-15 21:54:39 +0200

    Increase banner blur value

--- games/views.py
+++ games/views.py
@@ -82,7 +82,7 @@ def download_latest(request):
 
 def game_detail(request, slug):
     game = get_object_or_404(Game, slug=slug)
-    banner_options = {'crop': 'top', 'blur': '3x5'}
+    banner_options = {'crop': 'top', 'blur': '4x6'}
     banner_size = "940x352"
     return render(request, 'games/detail.html',
                   {'game': game,
