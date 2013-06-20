216f1c911b0bc6315075ba8690cc6468d26e92db
Mathieu Comandon <strycore@gmail.com>
2013-06-21 01:04:04 +0200

    Add latest games on home page

--- common/views.py
+++ common/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from common.models import News
-from games.models import FeaturedGame
+from games.models import FeaturedGame, Game
 
 
 def home(request):
     """Homepage view"""
     featured = FeaturedGame.objects.all()
     news = News.objects.all()[:5]
+    latest_games = Game.objects.published()[:5]
     download_type = "Ubuntu"
     return render(request, 'home.html',
                   {'featured_games': featured,
+                   'latest_games': latest_games,
                    'news': news,
                    'download_type': download_type})
 
