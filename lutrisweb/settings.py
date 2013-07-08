5a16d40e33491330144c858d75a0a939ab9dbbdc
Mathieu Comandon <strycore@gmail.com>
2013-07-08 16:42:51 +0200

    Add coverage config

--- lutrisweb/settings.py
+++ lutrisweb/settings.py
@@ -111,6 +111,12 @@ INSTALLED_APPS = (
 )
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ("--cover-erase", "--with-xunit", "--xunit-file=nosetests.xml",
+             "--with-xcoverage",
+             "--cover-package=games", "--cover-package=accounts",
+             "--cover-package=common"
+             "--xcoverage-file=coverage.xml")
+
 SOUTH_TESTS_MIGRATE = False
 LATEST_LUSTRIS_DEB = "http://lutris.net/releases/lutris_0.2.8_all.deb"
 
