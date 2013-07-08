f2f1c597f3c8dc748eff29a0d1b96612914ae8c7
Mathieu Comandon <strycore@gmail.com>
2013-07-08 17:13:11 +0200

    Move coverage config in local_settings

---
+++ config/local_settings_jenkins.py
@@ -0,0 +1,5 @@
+NOSE_ARGS = ("--cover-erase", "--with-xunit", "--xunit-file=nosetests.xml",
+             "--with-xcoverage",
+             "--cover-package=games", "--cover-package=accounts",
+             "--cover-package=common"
+             "--xcoverage-file=coverage.xml")
