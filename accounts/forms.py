01be02064c0d4fce512fa8b899f9001ba930426d
Mathieu Comandon <strycore@gmail.com>
2013-09-30 22:01:56 +0200

    Seems that I cannot subclass UserCreationForm with custom models

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,12 +26,70 @@ def get_bootstrap_helper(fields, submit_id, submit_label):
     return helper
 
 
-class RegistrationForm(UserCreationForm):
+class RegistrationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    error_messages = {
+        'duplicate_username': "A user with that username already exists.",
+        'password_mismatch': "The two password fields didn't match.",
+    }
+    username = forms.RegexField(
+        label="Username", max_length=30,
+        regex=r'^[\w.@+-]+$',
+        help_text=("Required. 30 characters or fewer. Letters, digits and "
+                   "@/./+/-/_ only."),
+        error_messages={
+            'invalid': ("This value may contain only letters, numbers and "
+                        "@/./+/-/_ characters.")
+        }
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput, max_length=64
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput,
+        max_length=64,
+        help_text="Enter the same password as above, for verification."
+    )
+
+    class Meta:
+        model = User
+        fields = ("username",)
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.helper = get_bootstrap_helper(
             ['username', 'password1', 'password2'], 'register', "Register"
         )
+
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'])
+        return password2
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
 class LoginForm(AuthenticationForm):
