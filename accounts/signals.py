c646aba4e10e79ecf8190ae0a903f22466d27534
Mathieu Comandon <strycore@gmail.com>
2013-11-27 23:32:01 +0100

    Add actual email sending

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,8 +1,12 @@
 import logging
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.db import models
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
+
 from tastypie.models import create_api_key
+
 from accounts import models as account_models
 LOGGER = logging.getLogger(__name__)
 
@@ -20,6 +24,8 @@ def create_library(sender, instance, created, **kwargs):
 def send_registration_email(sender, instance, created, **kwargs):
     LOGGER.info("Sending registration email for %s", instance)
     if created:
-        print instance.email
+        body = "Hello %s!\n Your account is now active." % instance.username
+        send_mail("Welcome to lutris.net", body, settings.DEFAULT_FROM_EMAIL,
+                  [instance.email])
 
 models.signals.post_save.connect(create_api_key, sender=account_models.User)
