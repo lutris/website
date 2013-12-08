7537aa7dd3392a0d29d3f75a06b51f35f5c8da36
Mathieu Comandon <strycore@gmail.com>
2013-12-08 20:07:56 +0100

    DAMETTE

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -35,21 +35,22 @@ def sync_steam_library(user_id):
     library = games.models.GameLibrary.objects.get(user=user)
     for game in steam_games:
         LOGGER.info("Adding %s to %s's library", game['name'], user.username)
+        if not game['img_icon_url']:
+            LOGGER.info("Game %s has no icon" % game['name'])
+            continue
         try:
             steam_game = games.models.Game.objects.get(steamid=game['appid'])
         except games.models.Game.DoesNotExist:
-            if not game['img_icon_url']:
-                LOGGER.info("Game %s has no icon" % game['name'])
-                continue
-
-            existing_games = games.models.Game.objects.filter(
-                slug=slugify(game['name'])[50:]
-            )
+            pass
+        else:
             try:
-                steam_game = existing_games[0]
-                LOGGER.info("%s already in database", steam_game)
+                steam_game = games.models.Game.objects.get(
+                    slug=slugify(game['name'])[50:]
+                )
                 steam_game.appid = game['appid']
                 steam_game.save()
-            except IndexError:
+            except games.models.Game.DoesNotExist:
+                pass
+            else:
                 steam_game = create_game(game)
         library.games.add(steam_game)
