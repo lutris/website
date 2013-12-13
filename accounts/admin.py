9200236a5d30c2429753b9a5419b9b75408c5b16
Mathieu Comandon <strycore@gmail.com>
2013-12-13 17:48:56 +0100

    Add UserAdmin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,9 @@
 from accounts import models
 from django.contrib import admin
 
-admin.site.register(models.User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'email')
+    search_fields = ('username', 'email')
+
+admin.site.register(models.User, UserAdmin)
