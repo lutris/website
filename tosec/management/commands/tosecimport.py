45bbf16da8336845aa12d27acd98bab7902fa142
Mathieu Comandon <strycore@gmail.com>
2014-05-21 12:34:21 +0200

    Display games with the tosecimport command

--- tosec/management/commands/tosecimport.py
+++ tosec/management/commands/tosecimport.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+from tosec.parser import TosecParser
 from django.core.management.base import BaseCommand
 
 
@@ -8,4 +10,14 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for filename in args:
             self.stdout.write("Importing %s" % filename)
-            self.stdout.write("TODO: write actual import")
+            with open(filename, 'r') as tosec_dat:
+                dat_contents = tosec_dat.readlines()
+
+            for l in dat_contents:
+                print l
+
+            tosec_parser = TosecParser(dat_contents)
+            tosec_parser.parse()
+
+            for game in tosec_parser.games:
+                pprint(game)
