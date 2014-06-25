5843317a118111e5d1dc125e61a343a08a45e808
Xodetaetl <dev@ovocean.com>
2014-06-25 20:35:36 +0200

    Fix games and latest games lists showing games with no installer

--- common/templatetags/lutris.py
+++ common/templatetags/lutris.py
@@ -7,6 +7,6 @@ register = template.Library()
 
 @register.inclusion_tag('includes/latest_games.html', takes_context=True)
 def latest_games(context):
-    games = models.Game.objects.published().order_by('-created')[:6]
+    games = models.Game.objects.with_installer().order_by('-created')[:6]
     context['latest_games'] = games
     return context
