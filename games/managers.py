f6ddb5b76265d7597568d6169ed877e04c565f4a
Xodetaetl <dev@ovocean.com>
2014-08-14 22:58:27 +0200

    Order screenshots by ascending upload time in the front-end
    So that it's easy to order them intentionally. :)
    ... Until we come up with a better ordering solution, with weights or
    something.

--- games/managers.py
+++ games/managers.py
@@ -3,4 +3,4 @@ from django.db.models import Manager
 
 class ScreenshotManager(Manager):
     def published(self):
-        return self.get_query_set().filter(published=True)
+        return self.get_query_set().filter(published=True).order_by('uploaded_at')
