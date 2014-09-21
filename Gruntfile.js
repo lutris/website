fe1695ad6da57b5fb47611638687dd6583df1a9b
Mathieu Comandon <strycore@gmail.com>
2014-09-21 22:24:52 +0200

    Get rid of Django_Select2 widgets, use bower versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     less: {
       css: {
         options: {
-            paths: ['components/bootstrap/less/', 'components/select2-bootstrap-css/lib/']
+            paths: ['components/bootstrap/less/']
         },
         files: {
           'public/css/lutris.css': ['common_static/css/lutris.less']
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
       min: {
         options: {
             yuicompress: true,
-            paths: ['components/bootstrap/less/', 'components/select2-bootstrap-css/lib/']
+            paths: ['components/bootstrap/less/']
         },
         files: {
             'public/css/lutris.min.css': ['common_static/css/lutris.less']
