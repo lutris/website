cc47ff320dcd7d8acd36e14bb5c1a2e13883a486
Mathieu Comandon <strycore@gmail.com>
2014-09-26 23:25:00 +0200

    Use different email host in production

--- lutrisweb/settings/production.py
+++ lutrisweb/settings/production.py
@@ -16,6 +16,11 @@ DATABASES = {
     }
 }
 
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_USE_TLS = True
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
