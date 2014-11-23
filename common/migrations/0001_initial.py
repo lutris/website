0c3909dcf7893e17a12fdc12a05925c020421dc6
Mathieu Comandon <strycore@gmail.com>
2014-11-23 18:27:06 +0100

    Add migration dependencies

--- common/migrations/0001_initial.py
+++ common/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('accounts', '0001_initial'),
+    )
+
     def forwards(self, orm):
         # Adding model 'News'
         db.create_table('news', (
--- common/migrations/0002_auto__add_upload.py
+++ common/migrations/0002_auto__add_upload.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('accounts', '0001_initial'),
+    )
+
     def forwards(self, orm):
         # Adding model 'Upload'
         db.create_table(u'common_upload', (
