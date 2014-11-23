a6fb2d32ec4099a183d3dd2902345b73f7460f86
Mathieu Comandon <strycore@gmail.com>
2014-11-23 23:54:06 +0100

    Add options for tosec scan destination

--- tosec/management/commands/tosecscan.py
+++ tosec/management/commands/tosecscan.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+from optparse import make_option
 from tosec.models import Rom, Game
 from django.core.management.base import BaseCommand
 
@@ -7,12 +8,21 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     args = '<folder>'
     help = 'Scan a folder for TOSEC roms'
+    option_list = BaseCommand.option_list + (
+        make_option('--destination', action='store', type="str"),
+        make_option('--create-set-folders', action='store_true',
+                    dest="create_set_folders"),
 
-    def handle(self, *args, **kwargs):
+    )
+
+    def handle(self, *args, **options):
         directory = args[0]
-        dest = os.path.join(directory, 'TOSEC')
+        dest = options.get('destination')
+        if not dest:
+            dest = os.path.join(directory, 'TOSEC')
         if not os.path.exists(dest):
             os.makedirs(dest)
+        create_set_folders = options['create_set_folders']
         self.stdout.write("Scanning %s" % directory)
         filenames = os.listdir(directory)
         total_files = len(filenames)
@@ -34,8 +44,14 @@ class Command(BaseCommand):
             self.stdout.write("[{} of {}] Found {}".format(index,
                                                            total_files,
                                                            rom.name))
-            new_path = os.path.join(dest, rom.name)
-            os.rename(abspath, new_path)
+            if create_set_folders:
+                set_path = os.path.join(dest, set_name)
+                if not os.path.exists(set_path):
+                    os.mkdir(set_path)
+                dest_path = os.path.join(set_path, rom.name)
+            else:
+                dest_path = os.path.join(dest, rom.name)
+            os.rename(abspath, dest_path)
 
         for set_name in tosec_sets:
             set_size = Game.objects.filter(category__name=set_name).count()
