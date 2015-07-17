304dac1817f93f36491b0c011fb83dddc356b119
Mathieu Comandon <strycore@gmail.com>
2015-07-17 14:49:48 -0700

    Add some weird migration

---
+++ common/migrations/0004_auto_20150717_2348.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0003_auto_20150717_2226'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='news',
+            name='content_markup_type',
+            field=models.CharField(default=b'restructuredtext', max_length=30, editable=False, choices=[(b'', b'--'), (b'html', b'html'), (b'plain', b'plain'), (b'restructuredtext', b'restructuredtext')]),
+        ),
+    ]
