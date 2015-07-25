6cf1ad21eb6a38ab069be5ff072e74959c3e2463
Mathieu Comandon <strycore@gmail.com>
2015-07-24 18:38:33 -0700

    Setup CSRF protecttion for Ajax requests

--- common_static/js/app.js
+++ common_static/js/app.js
@@ -1,6 +1,68 @@
 /* global $ */
 
 (function() {
+
+  /**
+   * Return the cookie value referenced by name
+   * @param {string} name - Name of the cookie to return
+   * @returns {string} value of the cookie
+   */
+  function getCookie(name) {
+    var cookieValue = null;
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = $.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+    return cookieValue;
+  }
+
+  /**
+   * Return true if the method does not require CSRF protection
+   * @param {string} method - Name of the method
+   * @returns {Boolean} True if the method is CSRF safe
+   */
+  function isCsrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+
+
+  /**
+   * Returns true if url is the same origin of current website
+   * @param {string} url - URL to test
+   * @returns {Boolean} Whether the url is same origin
+   */
+  function isSameOrigin(url) {
+    // test that a given url is a same-origin URL
+    // url could be relative or scheme relative or absolute
+    var host = document.location.host; // host + port
+    var protocol = document.location.protocol;
+    var srOrigin = '//' + host;
+    var origin = protocol + srOrigin;
+    // Allow absolute or scheme relative URLs to same origin
+    return (url === origin || url.slice(0, origin.length + 1) === origin + '/') ||
+      (url === srOrigin || url.slice(0, srOrigin.length + 1) === srOrigin + '/') ||
+      // or any other URL that isn't scheme relative or absolute i.e relative.
+      !(/^(\/\/|http:|https:).*/.test(url));
+  }
+
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+      if (!isCsrfSafeMethod(settings.type) && isSameOrigin(settings.url)) {
+        // Send the token to same-origin, relative URLs only.
+        // Send the token only if the method warrants CSRF protection
+        // Using the CSRFToken value acquired earlier
+        var csrftoken = getCookie('csrftoken');
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    }
+  });
+
   var setActiveMenu = function() {
     var menuIndex, url;
     url = window.location.pathname;
