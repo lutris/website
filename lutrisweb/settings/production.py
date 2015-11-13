cd4f9f5e91be14373a3d2f0c15623655c8355978
Mathieu Comandon <strycore@gmail.com>
2015-11-12 22:38:41 -0800

    Set email port in production

--- lutrisweb/settings/production.py
+++ lutrisweb/settings/production.py
@@ -23,6 +23,7 @@ EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
+EMAIL_PORT = 25
 
 TEMPLATE_LOADERS = (
     ('django.template.loaders.cached.Loader', (
