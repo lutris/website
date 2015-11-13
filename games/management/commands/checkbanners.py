0c0c5f4a89f30c3c6f2ed18f50ce238ab5a9e2bc
Mathieu Comandon <strycore@gmail.com>
2015-11-12 23:15:31 -0800

    More checks for check_banners

--- games/management/commands/checkbanners.py
+++ games/management/commands/checkbanners.py
@@ -9,9 +9,12 @@ class Command(BaseCommand):
         games = Game.objects.all()
         for game in games:
             icon_path = os.path.join(settings.MEDIA_ROOT, game.icon.name)
-            if not os.path.exists(icon_path):
+            if game.icon.name and not os.path.exists(icon_path):
                 print "%s is missing icon" % game
             banner_path = os.path.join(settings.MEDIA_ROOT,
                                        game.title_logo.name)
-            if not os.path.exists(banner_path):
-                print "%s is missing icon" % game
+            if game.title_logo.name and not os.path.exists(banner_path):
+                print "%s is missing banner" % game
+
+            if game.is_public and not game.title_logo.name:
+                print "%s has no banner" % game
