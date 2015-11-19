1ee223833b5d866628a9866b78884a29bcf10e8f
Mathieu Comandon <strycore@gmail.com>
2015-11-18 16:26:23 -0800

    This is going to be useless

--- games/drf_urls.py
+++ games/drf_urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.GameListView.as_view(),
         name='api_game_list'),
-    url(r'^/banners', views.GameBannersView.as_view(), name='api_banners'),
     url(r'^/(?P<slug>[\w\-]+)$', views.GameDetailView.as_view(),
         name='api_game_detail')
 )
