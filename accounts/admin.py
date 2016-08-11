c428b2f2b9d30721484838a5abe0014635371c87
Mathieu Comandon <strycore@gmail.com>
2016-08-11 13:56:56 -0700

    Add filters on UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'email', 'email_confirmed', 'steamid', 'website')
+    list_display = ('__unicode__', 'email', 'email_confirmed', 'is_staff',
+                    'steamid', 'website')
+    list_filter = ('is_staff', 'groups')
     search_fields = ('username', 'email')
 
 admin.site.register(models.User, UserAdmin)
