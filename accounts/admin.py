5abe1919d22809fdaec2615d193272263306469e
Mathieu Comandon <strycore@gmail.com>
2016-08-10 23:17:17 -0700

    Display more info in user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'email')
+    list_display = ('__unicode__', 'email', 'email_confirmed', 'steamid', 'website')
     search_fields = ('username', 'email')
 
 admin.site.register(models.User, UserAdmin)
