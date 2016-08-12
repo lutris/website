761ba162338b86bc16aa4b642cc51a297d5491d6
Mathieu Comandon <strycore@gmail.com>
2016-08-11 18:22:54 -0700

    Set the default number of items to 12 because it fits more nicely on the email

--- games/notifier.py
+++ games/notifier.py
@@ -1,19 +1,21 @@
 from games import models
 
+DEFAULT_COUNT = 12
 
-def get_unpublished_installers(count=10):
+
+def get_unpublished_installers(count=DEFAULT_COUNT):
     return models.Installer.objects.filter(published=False).order_by('?')[:count]
 
 
-def get_unpublished_screenshots(count=10):
+def get_unpublished_screenshots(count=DEFAULT_COUNT):
     return models.Screenshot.objects.filter(published=False).order_by('?')[:count]
 
 
-def get_unreviewed_game_submissions(count=10):
+def get_unreviewed_game_submissions(count=DEFAULT_COUNT):
     return models.GameSubmission.objects.filter(accepted_at__isnull=True).order_by('?')[:count]
 
 
-def get_installer_issues(count=10):
+def get_installer_issues(count=DEFAULT_COUNT):
     return models.InstallerIssue.objects.all().order_by('?')[:count]
 
 
