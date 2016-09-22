49e86f8f0f16ac5fe20cb9f91893f5aa5eee2237
Mathieu Comandon <strycore@gmail.com>
2016-09-22 10:40:07 -0700

    Remove tests for old api

--- games/tests/test_api.py
+++ games/tests/test_api.py
@@ -4,31 +4,6 @@ from . import factories
 import json
 
 
-class TestOldApi(TestCase):
-    def setUp(self):
-        game = factories.GameFactory
-        games = [game() for i in range(5)]
-        self.library = factories.GameLibraryFactory(games=games)
-        other_games = [game(name="Metroid"), game(name="Mario")]
-        self.other_library = factories.GameLibraryFactory(games=other_games)
-
-    def test_anonymous_user_cant_get_library(self):
-        response = self.client.get("/api/v1/library/")
-        self.assertEqual(response.status_code, 401)
-
-    def test_get_library(self):
-        user = self.library.user
-        self.assertTrue(user.api_key)
-
-        response = self.client.get("/api/v1/library/?username=%s&api_key=%s"
-                                   % (user.username, user.api_key.key))
-        self.assertEqual(response.status_code, 200)
-        library_games = json.loads(response.content)["objects"][0]['games']
-        game_slugs = [game['slug'] for game in library_games]
-        self.assertIn("quake", game_slugs)
-        self.assertNotIn("mario", game_slugs)
-
-
 class TestGameApi(TestCase):
     def setUp(self):
         self.num_games = 10
