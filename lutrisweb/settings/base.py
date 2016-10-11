5eed0f793c912be0285dca1edb68fbe84ec0536d
Mathieu Comandon <strycore@gmail.com>
2016-10-10 21:18:08 -0700

    Increase default page size to 250 objects for DRF requests

--- lutrisweb/settings/base.py
+++ lutrisweb/settings/base.py
@@ -210,7 +210,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 250,
 }
 
 # Logging
