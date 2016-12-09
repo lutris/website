9fb2500ee6c0a970a035eb348e1928c51198c3b6
Mathieu Comandon <strycore@gmail.com>
2016-12-09 03:45:21 -0800

    Get rid of active tab classes in main nav

--- common_static/js/app.js
+++ common_static/js/app.js
@@ -62,33 +62,4 @@
       }
     }
   });
-
-  function setActiveMenu() {
-    var menuIndex, url;
-    url = window.location.pathname;
-    $('#main-nav li').removeClass('active');
-    if (url === "/") {
-      menuIndex = 0;
-    } else if (url.substr(0, 6) === '/about') {
-      menuIndex = 1;
-    } else if (url.substr(0, 10) === '/downloads') {
-      menuIndex = 2;
-    } else if (url.substr(0, 6) === '/games') {
-      menuIndex = 3;
-    } else if (url.slice(-8) === 'library/') {
-      menuIndex = 5;
-    } else if (url.slice(-6) === 'login/') {
-      menuIndex = 5;
-    } else if (url.slice(-9) === 'register/') {
-      menuIndex = 6;
-    } else if (url.substr(0, 5) === '/user') {
-      menuIndex = 6;
-    }
-    return $('#main-nav li').eq(menuIndex).addClass('active');
-  };
-
-  $(function() {
-    return setActiveMenu();
-  });
-
 }).call(this);
