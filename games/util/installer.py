779f698b1e01b0afb5283641f54069e897cbb1a9
Mathieu Comandon <strycore@gmail.com>
2016-12-29 16:36:52 -0800

    Remove classes for the sake of classes in ScriptValidator

--- games/util/installer.py
+++ games/util/installer.py
@@ -1,8 +1,11 @@
+SUCCESS = (True, "")
+
+
 class ScriptValidator(object):
     def __init__(self, script):
         self.rules = [
-            FilesShouldBeArray(),
-            ScummVMGameHasGameID(),
+            files_is_an_array,
+            scummvm_has_gameid,
         ]
         self.script = script
         self.errors = []
@@ -10,31 +13,33 @@ class ScriptValidator(object):
 
     def is_valid(self):
         for rule in self.rules:
-            if not rule.is_valid(self.script):
-                self.errors.append(rule.message)
+            success, message = rule(self.script)
+            if not success:
+                self.errors.append(message)
                 self.valid = False
         return self.valid
 
 
-class FilesShouldBeArray(object):
-    def is_valid(self, script):
-        if 'files' in script:
-            if not isinstance(script['files'], list):
-                self.message = "'files' section should be an array."
-                return False
-        return True
+def files_is_an_array(script):
+    if 'files' in script:
+        if not isinstance(script['files'], list):
+            return (False, "'files' section should be an array.")
+    return SUCCESS
 
 
-class ScummVMGameHasGameID(object):
+class scummvm_has_gameid(object):
     def is_valid(self, script):
         runner = script.get('runner')
         if runner != 'scummvm':
-            return True
+            return SUCCESS
         if 'game' not in script:
-            self.message = ("Missing section 'game'")
-            return False
+            return (
+                False,
+                "Missing section 'game'"
+            )
         if 'game_id' not in script['game']:
-            self.message = ("ScummVM game should have a "
-                            "game identifier in the 'game' section")
-            return False
-        return True
+            return (
+                False,
+                "ScummVM game should have a game identifier in the 'game' section"
+            )
+        return SUCCESS
