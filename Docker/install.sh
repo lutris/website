05280f1b252356e4c9aff92fa64b71503e98e2ae
joonatoona <joonatoona@digitalfishfun.com>
2017-06-29 17:57:23 -0700

    Added docker

---
+++ Docker/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+chmod 755 $HOME/.nvm/nvm.sh
+source $HOME/.nvm/nvm.sh
+nvm install node
+npm install -g bower grunt-cli
+git clone https://github.com/lutris/website.git /app
+echo 'SECRET_KEY="jhkfghsldfkjghsldfh"' >> lutrisweb/settings/base.py
+sed -i -e "s/sudo//g" Makefile
+echo 'export DJANGO_SETTINGS_MODULE="lutrisweb.settings.local"' >> ~/.bashrc
+echo 'export USE_SQLITE=1' >> ~/.bashrc
+export DJANGO_SETTINGS_MODULE="lutrisweb.settings.local"
+export USE_SQLITE=1
+make setup
+make sysdeps
+make db
+make client
+make docs
+pip install Faker
