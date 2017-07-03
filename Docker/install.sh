79833e8e1ba0dafa836e33eb160851723dd138a2
Mathieu Comandon <strycore@gmail.com>
2017-07-03 10:18:01 -0700

    Set environment variables in Dockerfile

--- Docker/install.sh
+++ Docker/install.sh
@@ -5,13 +5,6 @@ chmod 755 $HOME/.nvm/nvm.sh
 source $HOME/.nvm/nvm.sh
 nvm install node
 npm install -g bower grunt-cli
-git clone https://github.com/lutris/website.git /app
-echo 'SECRET_KEY="jhkfghsldfkjghsldfh"' >> lutrisweb/settings/base.py
-sed -i -e "s/sudo//g" Makefile
-echo 'export DJANGO_SETTINGS_MODULE="lutrisweb.settings.local"' >> ~/.bashrc
-echo 'export USE_SQLITE=1' >> ~/.bashrc
-export DJANGO_SETTINGS_MODULE="lutrisweb.settings.local"
-export USE_SQLITE=1
 make setup
 make sysdeps
 make db
