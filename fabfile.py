2f9b861634a4db50d941507ba8ce2898de97b691
Mathieu Comandon <strycore@gmail.com>
2018-05-15 02:52:14 -0700

    New fabfile almost functional

--- fabfile.py
+++ fabfile.py
@@ -9,32 +9,42 @@ LUTRIS_REMOTE = 'git@github.com:lutris/website.git'
 DJANGO_SETTINGS_MODULE = 'lutrisweb.settings.production'
 
 
-def get_config(context=None):
-    env = 'staging'
-    if context and context.host == 'lutris.net':
-        env = 'production'
+def get_config(context):
+    try:
+        host = context.host
+    except AttributeError:
+        host = 'localhost'
 
+    if host == 'lutris.net':
+        env = 'production'
+    elif host == 'dev.lutris.net':
+        env = 'staging'
+    else:
+        env = 'local'
+
+    code_root = None
     if env == 'production':
-        domain = 'lutris.net'
         root = os.path.join('/srv/lutris')
         git_branch = 'master'
     elif env == 'staging':
-        domain = 'dev.lutris.net'
         root = os.path.join('/srv/lutris_staging')
         git_branch = 'py3'
     else:
-        raise ValueError("Unsupported environment %s" % env)
+        root = os.path.dirname(os.path.abspath(__file__))
+        code_root = root
+        git_branch = ''
 
     return {
         'root': root,
-        'code_root': os.path.join(root, domain),
-        'domain': domain,
-        'git_branch': git_branch
+        'code_root': code_root or os.path.join(root, host),
+        'domain': host,
+        'git_branch': git_branch,
+        'env': env
     }
 
 
 def activate(c):
-    config = get_config()
+    config = get_config(c)
     root = config['root']
     return c.prefix(
         'export DJANGO_SETTINGS_MODULE=%s && '
@@ -47,7 +57,7 @@ def activate(c):
 @task
 def touch_wsgi(c):
     """Touch wsgi file to trigger reload."""
-    config = get_config()
+    config = get_config(c)
     conf_dir = os.path.join(config['code_root'], 'config')
     with c.cd(conf_dir):
         c.run('touch lutrisweb.wsgi')
@@ -70,14 +80,13 @@ def test(c):
     config = get_config(c)
     with c.cd(config['code_root']):
         with activate(c):
-            c.run("python manage.py test games")
             c.run("python manage.py test accounts")
 
 
 @task
 def initial_setup(c):
     """Setup virtualenv"""
-    config = get_config()
+    config = get_config(c)
     c.sudo("mkdir -p %s" % config['root'])
     c.sudo("chown %s:%s %s" % (c.user, c.user, config['root']))
     c.sudo("apt install -y python3-venv")
@@ -87,15 +96,8 @@ def initial_setup(c):
 
 
 @task
-def check(c):
-    config = get_config()
-    with c.cd(config['root']):
-        c.run("ls -lah dev.lutris.net")
-
-
-@task
 def pip_list(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         with activate(c):
             c.run('pip list')
@@ -103,7 +105,7 @@ def pip_list(c):
 
 @task
 def requirements(c, environment='production'):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         with activate(c):
             c.run('pip install -U pip')
@@ -113,30 +115,24 @@ def requirements(c, environment='production'):
             )
 
 
+@task
 def update_celery(c):
-    config = get_config()
+    config = get_config(c)
     tempfile = "/tmp/lutrisweb-celery.conf"
     c.local('cp config/lutrisweb-celery.conf ' + tempfile)
     c.local('sed -i s#%%ROOT%%#%(root)s#g ' % config + tempfile)
     c.local('sed -i s/%%DOMAIN%%/%(domain)s/g ' % config + tempfile)
-    c.put(tempfile, '%(root)s' % config)
+    print('%(root)s' % config)
+    c.put(tempfile, remote='%(root)s' % config)
     c.sudo(
-        'cp %(root)s/%(domain)s-celery.conf /etc/supervisor/conf.d/' % config,
-        shell=False
+        'mv %(root)s/lutrisweb-celery.conf '
+        '/etc/supervisor/conf.d/%(domain)s-celery.conf' % config
     )
 
 
 @task
-def copy_local_settings(c):
-    config = get_config()
-    c.put('config/local_settings_%(environment)s.py' % config, config['code_root'])
-    with c.cd(config['code_root']):
-        c.run('mv local_settings_%(environment)s.py local_settings_template.py' % config)
-
-
-@task
 def migrate(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         with activate(c):
             c.run("./manage.py migrate")
@@ -144,33 +140,35 @@ def migrate(c):
 
 @task
 def pull(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         c.run("git checkout %s" % config['git_branch'])
         c.run("git pull")
+        c.run("git log")
 
 
+@task
 def npm(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         c.run("npm install -U bower")
         c.run("npm install")
 
 
 def bower(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         c.run("bower install")
 
 
 def grunt(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         c.run("grunt")
 
 
 def collect_static(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         with activate(c):
             c.run('./manage.py collectstatic --noinput')
@@ -179,7 +177,7 @@ def collect_static(c):
 def fix_perms(c, user='www-data', group=None):
     if not group:
         group = user
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         c.sudo('chown -R %s:%s static' % (user, group))
         c.sudo('chmod -R ug+w static')
@@ -187,25 +185,28 @@ def fix_perms(c, user='www-data', group=None):
         c.sudo('chmod -R ug+w media')
 
 
+@task
 def clean(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
-        c.run('find . -name "*.pyc" -delete')
+        c.run('py3clean .')
 
 
+@task
 def configtest(c):
     c.sudo("service nginx configtest")
 
 
+@task
 def authorize(c, ip):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         with activate(c):
             c.run('./manage.py authorize %s' % ip)
 
 
 def docs(c):
-    config = get_config()
+    config = get_config(c)
     with c.cd(config['code_root']):
         c.run("make client")
         with activate(c):
