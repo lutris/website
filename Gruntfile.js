7e145315d0f8accc3ae5bb0541d10ee29688129b
Mathieu Comandon <strycore@gmail.com>
2018-11-11 18:23:58 -0800

    Use new 'frontend' location for the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,32 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     copy: {
       main: {
         files: [
           {
             expand: true,
             flatten: true,
-            src: ['components/bootstrap/dist/fonts/*'],
-            dest: 'public/fonts/'
+            src: ["components/bootstrap/dist/fonts/*"],
+            dest: "public/fonts/"
           },
           {
             expand: true,
             flatten: true,
-            src: ['components/jcrop/css/Jcrop.gif'],
-            dest: 'public/css/'
+            src: ["components/jcrop/css/Jcrop.gif"],
+            dest: "public/css/"
           },
           {
             expand: true,
             flatten: true,
-            src: ['components/ace-builds/src-noconflict/*'],
-            dest: 'public/ace'
+            src: ["components/ace-builds/src-noconflict/*"],
+            dest: "public/ace"
           },
           {
             expand: true,
             flatten: true,
-            src: ['components/ace-builds/src-noconflict/snippets/*'],
-            dest: 'public/ace/snippets'
+            src: ["components/ace-builds/src-noconflict/snippets/*"],
+            dest: "public/ace/snippets"
           }
         ]
       }
@@ -34,132 +34,124 @@ module.exports = function(grunt) {
     less: {
       css: {
         options: {
-            sourceMap: true,
-            strictImports: true,
-            strictMath: true,
-            strictUnits: true,
-            paths: ['components/bootstrap/less/']
+          sourceMap: true,
+          strictImports: true,
+          strictMath: true,
+          strictUnits: true,
+          paths: ["components/bootstrap/less/"]
         },
         files: {
-          'public/css/lutris.css': ['common_static/css/lutris.less']
+          "public/css/lutris.css": ["frontend/css/lutris.less"]
         }
       },
       min: {
         options: {
-            yuicompress: true,
-            sourceMap: true,
-            strictImports: true,
-            strictMath: true,
-            strictUnits: true,
-            paths: ['components/bootstrap/less/']
+          yuicompress: true,
+          sourceMap: true,
+          strictImports: true,
+          strictMath: true,
+          strictUnits: true,
+          paths: ["components/bootstrap/less/"]
         },
         files: {
-            'public/css/lutris.min.css': ['common_static/css/lutris.less']
+          "public/css/lutris.min.css": ["frontend/css/lutris.less"]
         }
       }
     },
     concat: {
       css: {
         src: [
-          'components/jcrop/css/jquery.Jcrop.css',
-          'components/jcrop-fileinput/dist/jcrop-fileinput.css'
+          "components/jcrop/css/jquery.Jcrop.css",
+          "components/jcrop-fileinput/dist/jcrop-fileinput.css"
         ],
-        dest: 'public/css/libs.css',
+        dest: "public/css/libs.css",
         nonull: true
       },
       app: {
-        src: ['common_static/js/*.js'],
-        dest: 'public/js/app.js',
+        src: ["frontend/js/*.js"],
+        dest: "public/js/app.js",
         nonull: true
       },
       js: {
         src: [
-          'components/jquery/dist/jquery.js',
-          'components/blueimp-canvas-to-blob/js/canvas-to-blob.js',
-          'components/bootstrap/dist/js/bootstrap.js',
-          'components/jcrop/js/jquery.Jcrop.js',
-          'components/jcrop-fileinput/dist/jcrop-fileinput.js'
+          "components/jquery/dist/jquery.js",
+          "components/blueimp-canvas-to-blob/js/canvas-to-blob.js",
+          "components/bootstrap/dist/js/bootstrap.js",
+          "components/jcrop/js/jquery.Jcrop.js",
+          "components/jcrop-fileinput/dist/jcrop-fileinput.js"
         ],
-        dest: 'public/js/libs.js',
+        dest: "public/js/libs.js",
         nonull: true
       },
       select2Js: {
-        src: [
-          'components/select2/dist/js/select2.full.min.js',
-        ],
-        dest: 'public/js/select2.min.js',
+        src: ["components/select2/dist/js/select2.full.min.js"],
+        dest: "public/js/select2.min.js",
         nonull: true
       },
       select2Css: {
         src: [
-          'components/select2/dist/css/select2.min.css',
-          'components/select2-bootstrap-theme/dist/select2-bootstrap.min.css'
+          "components/select2/dist/css/select2.min.css",
+          "components/select2-bootstrap-theme/dist/select2-bootstrap.min.css"
         ],
-        dest: 'public/css/select2.min.css',
+        dest: "public/css/select2.min.css",
         nonull: true
       },
       emailCss: {
-        src: [
-          'common_static/css/email.css'
-        ],
-        dest: 'public/css/email.css',
+        src: ["frontend/css/email.css"],
+        dest: "public/css/email.css",
         nonull: true
-      },
-
+      }
     },
     cssmin: {
       dist: {
         files: {
-          'public/css/libs.min.css': ['public/css/libs.css'],
-          'public/css/email.min.css': ['public/css/email.css']
+          "public/css/libs.min.css": ["public/css/libs.css"],
+          "public/css/email.min.css": ["public/css/email.css"]
         }
       }
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner:
+          '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
       },
       dist: {
         files: {
-          'public/js/libs.min.js': ['public/js/libs.js'],
-          'public/js/app.min.js': ['public/js/app.js']
+          "public/js/libs.min.js": ["public/js/libs.js"],
+          "public/js/app.min.js": ["public/js/app.js"]
         }
       }
     },
     watch: {
       less: {
-        files: 'common_static/css/**/*.less',
-        tasks: ['less:css', 'less:min']
+        files: "frontend/css/**/*.less",
+        tasks: ["less:css", "less:min"]
       },
       templates: {
-        files: ['templates/**/*.html']
+        files: ["templates/**/*.html"]
       }
     },
     browserSync: {
       dev: {
         bsFiles: {
-          src: [
-            'templates/**',
-            'public/**'
-          ]
+          src: ["templates/**", "public/**"]
         },
         options: {
-          proxy: 'localhost:8000',
+          proxy: "localhost:8000",
           watchTask: true
         }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-less");
 
-  grunt.registerTask('default', ['copy', 'less', 'concat', 'cssmin', 'uglify']);
-  grunt.registerTask('serve', ['browserSync', 'watch']);
+  grunt.registerTask("default", ["copy", "less", "concat", "cssmin", "uglify"]);
+  grunt.registerTask("serve", ["browserSync", "watch"]);
 };
-
