7942c5a4c9e7a1ab9ca77d478de61cd5c9d47a60
Mathieu Comandon <strycore@gmail.com>
2018-11-11 18:23:13 -0800

    Add Vue project

---
+++ frontend/vue/src/components/InstallerIssues.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <span>Issues</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InstallerIssues',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
