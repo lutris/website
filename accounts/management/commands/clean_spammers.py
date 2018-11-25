ac08eb1cf430e876b774fb78a8113076a77abe1b
Mathieu Comandon <strycore@gmail.com>
2018-11-25 09:09:42 -0800

    Update clear_spammers syntax

--- accounts/management/commands/clean_spammers.py
+++ accounts/management/commands/clean_spammers.py
@@ -1,10 +1,11 @@
-from django.db.models import Q, Count
+from django.db.models import Count
 from django.core.management.base import BaseCommand
 from accounts.models import User
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **_options):
+        """Delete unconfirmed users with a website and no games"""
         users = (
             User.objects
             .annotate(game_count=Count('gamelibrary__games'))
@@ -13,11 +14,10 @@ class Command(BaseCommand):
                 game_count=0
             )
             .exclude(website__exact='')
-
         )
 
         cleared_users = len(users)
         for user in users:
-            print user, user.website
+            print("Deleting %s (website: %s)" % (user, user.website))
             user.delete()
-        print "Cleared %d users" % cleared_users
+        print("Cleared %d users" % cleared_users)
