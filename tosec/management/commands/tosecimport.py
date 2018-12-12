3b79e9d661f591b760df9a1e221f246a9b9d95e1
Mathieu Comandon <strycore@gmail.com>
2018-12-11 17:59:09 -0800

    Move import_tosec_database to a utils module

--- tosec/management/commands/tosecimport.py
+++ tosec/management/commands/tosecimport.py
@@ -3,50 +3,13 @@ import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from tosec import models
-from tosec.parser import TosecParser
+from tosec.utils import import_tosec_database
 
 
 class Command(BaseCommand):
     """Import a TOSEC dump to the Lutris database"""
     args = '<tosec_catalog tosec_catalog ...>'
     help = "Import Tosec catalog files in the database"
-
-    @staticmethod
-    def import_tosec_database(filename):
-        """Import a TOSEC database referenced by filename"""
-        with open(filename, 'r') as tosec_dat:
-            dat_contents = tosec_dat.readlines()
-
-        tosec_parser = TosecParser(dat_contents)
-        tosec_parser.parse()
-
-        category = models.Category(
-            name=tosec_parser.headers['name'],
-            description=tosec_parser.headers['description'],
-            category=tosec_parser.headers['category'],
-            version=tosec_parser.headers['version'],
-            author=tosec_parser.headers['author'],
-        )
-        category.save()
-
-        for game in tosec_parser.games:
-            game_row = models.Game(
-                category=category,
-                name=game['name'],
-                description=game['description'],
-            )
-            game_row.save()
-            rom = game['rom']
-            rom_row = models.Rom(
-                game=game_row,
-                name=rom['name'],
-                size=rom['size'],
-                crc=rom['crc'],
-                md5=rom.get('md5', ''),
-                sha1=rom.get('sha1', ''),
-            )
-            rom_row.save()
 
     def handle(self, *args, **_options):
         if args:
@@ -63,4 +26,4 @@ class Command(BaseCommand):
             self.stdout.write("Importing {} [{} of {}]".format(filename,
                                                                index,
                                                                total_files))
-            self.import_tosec_database(filename)
+            import_tosec_database(filename)
---
+++ tosec/utils.py
@@ -0,0 +1,39 @@
+"""Utilities for handling TOSEC files"""
+from tosec import models
+from tosec.parser import TosecParser
+
+
+def import_tosec_database(filename):
+    """Import a TOSEC database referenced by filename"""
+    with open(filename, 'r') as tosec_dat:
+        dat_contents = tosec_dat.readlines()
+
+    tosec_parser = TosecParser(dat_contents)
+    tosec_parser.parse()
+
+    category = models.Category(
+        name=tosec_parser.headers['name'],
+        description=tosec_parser.headers['description'],
+        category=tosec_parser.headers['category'],
+        version=tosec_parser.headers['version'],
+        author=tosec_parser.headers['author'],
+    )
+    category.save()
+
+    for game in tosec_parser.games:
+        game_row = models.Game(
+            category=category,
+            name=game['name'],
+            description=game['description'],
+        )
+        game_row.save()
+        rom = game['rom']
+        rom_row = models.Rom(
+            game=game_row,
+            name=rom['name'],
+            size=rom['size'],
+            crc=rom['crc'],
+            md5=rom.get('md5', ''),
+            sha1=rom.get('sha1', ''),
+        )
+        rom_row.save()
