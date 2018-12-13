adf587fa0c24952c3dc565b3a80569d0ce5a8e1a
Mathieu Comandon <strycore@gmail.com>
2018-12-12 22:38:28 -0800

    Create Lutris games from sync_gog_games script

--- scripts/sync_gog_games.py
+++ scripts/sync_gog_games.py
@@ -4,12 +4,37 @@ import re
 import json
 import time
 import logging
+from datetime import datetime
+
 import requests
+from django.db.utils import IntegrityError
 
-from games.models import Game
+from games.models import Game, Genre
+from platforms.models import Platform
+from common.util import slugify
 
-GOG_CACHE_PATH = 'gog_cache'
+
+GOG_CACHE_PATH = 'gog-cache'
+
 LOGGER = logging.getLogger(__name__)
+
+
+def clean_name(name):
+    """Remove some special characters from game titles to allow for easier comparison"""
+    return name.replace('™', '').replace('®', '').strip()
+
+
+def clean_gog_slug(gog_game):
+    """Return a lutris like slug from a GOG game"""
+    gog_slug = gog_game['slug']
+    gog_title = clean_name(gog_game['title']).lower()
+    cleaned_slug = gog_slug.replace('_', '-')
+    if gog_slug.endswith('_a') and gog_title.startswith('a '):
+        cleaned_slug = 'a-' + cleaned_slug[:-2]
+    if gog_slug.endswith('_the') and gog_title.startswith('the '):
+        cleaned_slug = 'the-' + cleaned_slug[:-4]
+    LOGGER.info(cleaned_slug)
+    return cleaned_slug
 
 
 def fetch_gog_games_page(page):
@@ -33,25 +58,28 @@ def cache_gog_games():
 
 
 def iter_gog_games():
-    nopes = ("_soundtrack", "_ost", "_extras", "_pack", "_dlc")
+    """Iterate through all GOG games from their API"""
+    excluded_suffixes = (
+        "_soundtrack",
+        "_soundtrack_remastered",
+        "_ost",
+        "_extras",
+        "_demo",
+        "_pack",
+        "_dlc",
+        "_season_pass"
+    )
     num_pages = len([f for f in os.listdir(GOG_CACHE_PATH) if re.match(r'(\d+)\.json', f)])
     for page in range(1, num_pages + 1):
         with open(os.path.join(GOG_CACHE_PATH, "{}.json".format(page))) as json_file:
             api_results = json.load(json_file)
             for product in api_results['products']:
-                if not product["slug"].endswith(nopes):
+                if not product["slug"].endswith(excluded_suffixes):
                     yield product
 
 
-def clean_name(name):
-    return name.lower().replace('™', '').replace('®', '').replace(':', '').strip()
-
-
-def compare_names(name_1, name_2):
-    return clean_name(name_1) == clean_name(name_2)
-
-
 def iter_lutris_games_by_gog_slug():
+    """Iterate through Lutris games that have a valid GOG slug"""
     for gog_game in iter_gog_games():
         for game in Game.objects.filter(gogslug=gog_game["slug"]):
             yield (game, gog_game)
@@ -59,15 +87,17 @@ def iter_lutris_games_by_gog_slug():
 
 def iter_lutris_games_by_lutris_slug():
     for gog_game in iter_gog_games():
-        for game in Game.objects.filter(slug=gog_game["slug"].replace("_", "-")):
+        for game in Game.objects.filter(slug=clean_gog_slug(gog_game)):
             if not game.gogid:
                 yield (game, gog_game)
 
 
 def iter_orphan_gog_games():
     for gog_game in iter_gog_games():
-        games = Game.objects.filter(gogid=gog_game["id"])
-        if not games:
+        if gog_game["type"] != 1:
+            # Exclude special editions and DLCs
+            continue
+        if not Game.objects.filter(gogid=gog_game["id"]).count():
             yield gog_game
 
 
@@ -76,8 +106,6 @@ def sync_slugs_with_ids():
     slug_counter = 0
     id_counter = 0
     for game, gog_game in iter_lutris_games_by_gog_slug():
-        if not compare_names(game.name, gog_game["title"]):
-            LOGGER.info("Title is different: '%s' and '%s'", game.name, gog_game["title"])
         try:
             game = Game.objects.get(gogid=gog_game["id"])
         except Game.DoesNotExist:
@@ -102,6 +130,44 @@ def sync_ids_by_slug():
     LOGGER.info("Synced %s games", game_counter)
 
 
+def create_game(gog_game):
+    """Creates a Lutris game from a GOG game"""
+    name = clean_name(gog_game["title"])
+    try:
+        game = Game.objects.create(
+            name=name,
+            slug=slugify(name),
+            gogid=gog_game["id"],
+            gogslug=gog_game["slug"],
+            is_public=True
+        )
+    except IntegrityError:
+        LOGGER.warning("Game %s is already in Lutris!", slugify(name))
+        game = Game.objects.get(slug=slugify(name))
+        game.gogid = gog_game["id"]
+        game.gogslug = gog_game["slug"]
+    game.set_logo_from_gog(gog_game)
+    if gog_game["worksOn"]["Linux"]:
+        platform = Platform.objects.get(slug='linux')
+    else:
+        platform = Platform.objects.get(slug='windows')
+    game.platforms.add(platform)
+    for gog_genre in gog_game["genres"]:
+        genre, created = Genre.objects.get_or_create(slug=slugify(gog_genre))
+        if created:
+            genre.name = gog_genre
+            LOGGER.info("Created genre %s", genre.name)
+            genre.save()
+        game.genres.add(genre)
+
+    if gog_game["releaseDate"]:
+        release_date = datetime.fromtimestamp(gog_game["releaseDate"])
+        game.year = release_date.year
+
+    game.save()
+    return game
+
+
 def run():
     """Read cached GOG files and matches the games against Lutris games"""
 
@@ -112,8 +178,13 @@ def run():
     sync_slugs_with_ids()
     sync_ids_by_slug()
 
-    # i = 0
-    # for gog_game in iter_orphan_gog_games():
-    #     print("https://gog.com" + gog_game["url"])
-    #     i += 1
-    # print(i)
+    i = 0
+    for gog_game in iter_orphan_gog_games():
+        game = create_game(gog_game)
+        if not game:
+            continue
+        if not game.title_logo:
+            raise RuntimeError("No")
+        LOGGER.info("Created game %s", game)
+        i += 1
+    LOGGER.info("%d games created", i)
