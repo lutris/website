47f0aa4276a9585513fde369e2d101ba9f652e06
Mathieu Comandon <strycore@gmail.com>
2019-02-07 22:49:11 -0800

    Re-format accounts.forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,43 +17,44 @@ class RegistrationForm(forms.ModelForm):
     A form that creates a user, with no privileges, from the given username and
     password.
     """
+
     error_messages = {
-        'duplicate_username': "A user with that username already exists.",
-        'password_mismatch': "The two password fields didn't match.",
+        "duplicate_username": "A user with that username already exists.",
+        "password_mismatch": "The two password fields didn't match.",
     }
     username = forms.RegexField(
-        label="Username", max_length=30,
-        regex=r'^[\w.@+-]+$',
+        label="Username",
+        max_length=30,
+        regex=r"^[\w.@+-]+$",
         help_text=("30 characters max."),
         error_messages={
-            'invalid': ("This value may contain only letters, numbers and "
-                        "@/./+/-/_ characters.")
-        }
+            "invalid": (
+                "This value may contain only letters, numbers and "
+                "@/./+/-/_ characters."
+            )
+        },
     )
-    email = forms.EmailField(
-        label="Email address"
-    )
+    email = forms.EmailField(label="Email address")
     password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput, max_length=64
+        label="Password", widget=forms.PasswordInput, max_length=64
     )
     password2 = forms.CharField(
         label="Password confirmation",
         widget=forms.PasswordInput,
         max_length=64,
-        help_text="Enter the same password as above, for verification."
+        help_text="Enter the same password as above, for verification.",
     )
 
     class Meta:
         """Model and field definitions"""
+
         model = User
         fields = ("username", "email")
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.helper = get_bootstrap_helper(
-            ['username', 'email', 'password1', 'password2'],
-            'register', "Register"
+            ["username", "email", "password1", "password2"], "register", "Register"
         )
 
     def clean_username(self):
@@ -66,14 +67,13 @@ class RegistrationForm(forms.ModelForm):
             return username
         except User.MultipleObjectsReturned:
             LOGGER.error("Mutiple users with username: %s", username)
-        raise forms.ValidationError(self.error_messages['duplicate_username'])
+        raise forms.ValidationError(self.error_messages["duplicate_username"])
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(
-                self.error_messages['password_mismatch'])
+            raise forms.ValidationError(self.error_messages["password_mismatch"])
         return password2
 
     def save(self, commit=True):
@@ -89,22 +89,21 @@ class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
         self.helper = get_bootstrap_helper(
-            ['username', 'password'], 'signin', "Sign in"
+            ["username", "password"], "signin", "Sign in"
         )
 
 
 class ProfileForm(forms.ModelForm):
     class Meta(object):
         model = User
-        fields = ('website', 'avatar', 'email')
+        fields = ("website", "avatar", "email")
 
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
-        self.helper = get_bootstrap_helper(list(self.Meta.fields),
-                                           'save', "Save")
+        self.helper = get_bootstrap_helper(list(self.Meta.fields), "save", "Save")
 
     def save(self, commit=True):
-        if 'email' in self.changed_data:
+        if "email" in self.changed_data:
             self.instance.email_confirmed = False
         return super(ProfileForm, self).save(commit=commit)
 
