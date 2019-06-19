4710bb37ff80a424803a8f99181ab9a2b31862d2
Mathieu Comandon <strycore@gmail.com>
2019-06-18 23:35:03 -0700

    Remove sudo modification in Docker

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-chmod 755 $HOME/.nvm/nvm.sh
+set -e
+
+if [ ! -d $HOME/.nvm/nvm.sh ]; then
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+    chmod 755 $HOME/.nvm/nvm.sh
+fi
 source $HOME/.nvm/nvm.sh
 nvm install node
 npm install -g bower grunt-cli
-sed -i -e "s/sudo//g" Makefile
 make setup
 make deps
 make sysdeps
