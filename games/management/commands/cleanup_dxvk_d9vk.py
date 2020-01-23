7e71e9419871e507cac547a225b7cad7c168e36c
Mathieu Comandon <strycore@gmail.com>
2020-01-22 18:25:13 -0800

    Fix d9vk cleanup script

--- games/management/commands/cleanup_dxvk_d9vk.py
+++ games/management/commands/cleanup_dxvk_d9vk.py
@@ -3,7 +3,6 @@ all D9VK attributes and dxvk_version from installers."""
 import logging
 
 from django.core.management.base import BaseCommand
-from django.db.models import Q
 
 from common.util import load_yaml, dump_yaml
 from games.models import Installer
@@ -19,54 +18,51 @@ class Command(BaseCommand):
         parser.add_argument(
             '--dry-run',
             action='store_true',
-            type=bool,
             dest='dry_run',
             help="don't write to the database"
         )
 
     @staticmethod
     def clean_up_dxvk_d9vk(script, slug):
-        not_found = object()
-
         try:
-            wine_config = script.get("wine", not_found)
-            if wine_config is not_found:
+            wine_config = script.get("wine")
+            if not wine_config:
                 return False
         except AttributeError:
             LOGGER.error("The script %s is invalid", slug)
             return False
 
         try:
-            d9vk_version = wine_config.get("d9vk_version", not_found)
-            dxvk_version = wine_config.get("dxvk_version", not_found)
+            d9vk_version = wine_config.get("d9vk_version")
+            dxvk_version = wine_config.get("dxvk_version")
         except AttributeError:
             LOGGER.error("The script %s is invalid", slug)
             return False
 
         changed = False
 
-        if d9vk_version is not not_found:
+        if d9vk_version:
             LOGGER.info("Removing D9VK version %s from %s", d9vk_version, slug)
             del wine_config["d9vk_version"]
             changed = True
-        if dxvk_version is not not_found:
+        if dxvk_version:
             LOGGER.info("Removing DXVK version %s from %s", dxvk_version, slug)
             del wine_config["dxvk_version"]
             changed = True
 
         try:
-            d9vk = wine_config.get("d9vk", not_found)
-            if d9vk is not_found:
+            d9vk = wine_config.get("d9vk")
+            if not d9vk:
                 return changed
-            dxvk = wine_config.get("dxvk", not_found)
+            dxvk = wine_config.get("dxvk")
         except AttributeError:
             LOGGER.error("The script %s is invalid", slug)
             return changed
 
-        if dxvk is not_found or not dxvk:
+        if not dxvk:
             LOGGER.info(
                 "Changing DXVK from %s to %s on %s",
-                dxvk if dxvk is not not_found else "<undefined>",
+                dxvk if dxvk else "nothing",
                 d9vk,
                 slug
             )
@@ -80,44 +76,18 @@ class Command(BaseCommand):
         """Enables DXVK where D9VK was active before and removes
         all D9VK attributes and dxvk_version from installers."""
 
-        # Get dry run flag from options.
         dry_run = options.get('dry_run')
 
-        # Search for installers that have a "d9vk", "d9vk_version", or
-        # "dxvk_version" field specified.
-        installers = Installer.objects.filter(
-            (  # JSON format
-                Q(content__icontains=',"wine":{')
-                & (
-                    Q(content__icontains='"d9vk":')
-                    | Q(content__icontains='"d9vk_version":')
-                    | Q(content__icontains='"dxvk_version":')
-                )
-            ) | (  # YAML format
-                Q(content__icontains=r"\nwine:\n  ")
-                & (
-                    Q(content__icontains=r"\n  d9vk: ")
-                    | Q(content__icontains=r"\n  d9vk_version: ")
-                    | Q(content__icontains=r"\n  dxvk_version: ")
-                )
-            )
-        )
-        # For each of those installers:
+        installers = Installer.objects.filter(content__icontains="d9vk:")
         for installer in installers:
-            # Parse the installer content.
             script = load_yaml(installer.content)
-
-            # Run the clean up function on the script.
             changed = Command.clean_up_dxvk_d9vk(script, installer.slug)
-            # If the script hasn't been changed, there's
-            # no need to save it.
             if not changed:
                 continue
-
-            # Serialize the new installer content.
             installer.content = dump_yaml(script)
-
-            # Save the new installer in the database.
-            LOGGER.info("Updating installer %s", installer)
-            if not dry_run:
+            if dry_run:
+                LOGGER.info("Dry run only, not saving installer %s", installer)
+            else:
+                LOGGER.info("Saving installer %s", installer)
                 installer.save()
+
