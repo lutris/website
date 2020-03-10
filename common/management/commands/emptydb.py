6482c3a3a2814de78ef7ff60c67a1936119babe0
Mathieu Comandon <strycore@gmail.com>
2020-03-09 17:47:45 -0700

    Fix syntax errors in emptydb script

--- common/management/commands/emptydb.py
+++ common/management/commands/emptydb.py
@@ -13,32 +13,12 @@ class Command(BaseCommand):
 
         command_output.seek(0)
         flush_sql = command_output.read().strip()
-        print flush_sql
-        flush_sql = """DELETE FROM "auth_permission";
-DELETE FROM "auth_group";
-DELETE FROM "django_site";
-DELETE FROM "accounts_user_user_permissions";
-DELETE FROM "django_content_type";
-DELETE FROM "django_session";
-DELETE FROM "accounts_user";
-DELETE FROM "django_openid_auth_useropenid";
-DELETE FROM "django_admin_log";
-DELETE FROM "auth_group_permissions";
-DELETE FROM "django_openid_auth_nonce";
-DELETE FROM "news";
-DELETE FROM "thumbnail_kvstore";
-DELETE FROM "django_select2_keymap";
-DELETE FROM "django_openid_auth_association";
-DELETE FROM "south_migrationhistory";
-DELETE FROM "accounts_user_groups";
-DELETE FROM "common_upload";""".split('\n')
 
         # execute the sql
         # from: https://docs.djangoproject.com/en/dev/topics/db/sql/#executing-custom-sql-directly
         cursor = connection.cursor()
         for statement in flush_sql:
-            print statement
-            print type(statement)
+            print(statement)
+            print(type(statement))
             cursor.execute(statement)
-
-        print "db has been reset"
+        print("db has been reset")
