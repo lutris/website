913ff820d594cc990b956a32a2028fe9dea19516
Mathieu Comandon <strycore@gmail.com>
2020-04-23 16:42:33 -0700

    Cleanup common.admin

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,4 @@
+"""Admin configuration for common"""
 from django.contrib import admin
 
 from common import models
@@ -5,13 +6,14 @@ from common import forms
 
 
 class NewsAdmin(admin.ModelAdmin):
+    """Admin configuration for News"""
     form = forms.NewsForm
     list_display = ('__str__', 'publish_date')
     search_fields = ('title', 'content')
 
 
-# pylint: disable=W0613
-def validate_upload(modeladmin, request, queryset):
+def validate_upload(_modeladmin, _request, queryset):
+    """Admin action to validate uploads in a queryset"""
     for upload in queryset.all():
         upload.validate()
 
@@ -20,6 +22,7 @@ validate_upload.short_description = "Move uploads to destination"
 
 
 class UploadAdmin(admin.ModelAdmin):
+    """Admin configuration for Uploads"""
     list_display = ('__str__', 'destination', 'uploaded_by', 'uploaded_at')
     actions = [validate_upload]
     readonly_fields = ['uploaded_by', 'uploaded_at']
