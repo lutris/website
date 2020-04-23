886a39c46ba3d8937023b7df2203bb1d27f8887e
Mathieu Comandon <strycore@gmail.com>
2020-04-23 14:18:22 -0700

    Move spam control utilities to a module

---
+++ accounts/spam_control.py
@@ -0,0 +1,37 @@
+"""Functions to get rid of spam accounts"""
+from django.db.models import Count
+from accounts.models import User
+
+
+def get_no_games_with_website():
+    """Return users that have not confirmed their account but have games"""
+    return (
+        User.objects
+        .annotate(game_count=Count('gamelibrary__games'))
+        .filter(
+            email_confirmed=False,
+            game_count=0
+        )
+        .exclude(website__exact='')
+    )
+
+
+def get_spam_avatar_users():
+    """Return users that have a avatar matching what spam users use"""
+    return (
+        User.objects
+        .annotate(game_count=Count('gamelibrary__games'))
+        .filter(
+            avatar__regex=r"/\d{2,3}_.{7}.gif",
+            game_count=0,
+        )
+    )
+
+
+def clear_users(users):
+    """Delete a list of users"""
+    cleared_users = len(users)
+    for user in users:
+        print("Deleting %s (website: %s)" % (user, user.website))
+        user.delete()
+    print("Cleared %d users" % cleared_users)
