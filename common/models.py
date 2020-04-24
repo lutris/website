2f09fa31aa97cade3ce41a03aff699017ba5f0c1
Mathieu Comandon <strycore@gmail.com>
2020-04-23 21:24:45 -0700

    Add Spaces upload feature to upload form

--- common/models.py
+++ common/models.py
@@ -9,6 +9,7 @@ from django.urls import reverse
 from markupfield.fields import MarkupField
 
 from common.util import slugify
+from common.spaces import SpacesBucket
 
 
 class News(models.Model):
@@ -43,7 +44,7 @@ class News(models.Model):
 class Upload(models.Model):
     """References to user uploaded files"""
     uploaded_file = models.FileField(upload_to='uploads')
-    destination = models.CharField(max_length=256)
+    destination = models.CharField("destination path", max_length=256)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     hosting = models.CharField(
@@ -62,20 +63,24 @@ class Upload(models.Model):
             return os.path.join(settings.MEDIA_ROOT, self.uploaded_file.name)
         return None
 
-    def move_to_local_hosting(self, ):
+    def move_to_local_hosting(self):
         """Move the file to its destination"""
-        destination = os.path.join(settings.FILES_ROOT, self.destination)
-        shutil.move(self.source_path, destination)
-
-    def validate(self):
-        """Validate an upload and move it to its destination"""
         destination = os.path.join(settings.FILES_ROOT, self.destination)
         if os.path.exists(destination):
             raise IOError("Can't overwrite files")
         if not os.path.exists(os.path.dirname(destination)):
             os.makedirs(os.path.dirname(destination))
-        if self.hosting == "local":
-            self.move_to_local_hosting()
+        shutil.move(self.source_path, destination)
+
+    def upload_to_spaces(self):
+        """Upload the file to Digital Ocean Spaces"""
+        space = SpacesBucket()
+        space.upload(self.source_path, self.destination, public=True)
+
+    def validate(self):
+        """Validate an upload and move it to its destination"""
+        if self.hosting == "spaces":
+            self.upload_to_spaces()
         else:
-            raise NotImplementedError("Spaces support coming")
+            self.move_to_local_hosting()
         self.delete()
