641473c4c1f594d02e995df80d0dbd007d56b774
Mathieu Comandon <strycore@gmail.com>
2020-04-29 14:48:37 -0700

    Remove start slashes in games routes

--- games/urls/games.py
+++ games/urls/games.py
@@ -6,8 +6,8 @@ from games.views import games as views
 
 urlpatterns = [
     path('', views.GameListView.as_view(), name='api_game_list'),
-    path('/stats', views.GameStatsView.as_view(), name='api_game_stats'),
-    path('/library/<username>', views.GameLibraryView.as_view(), name='api_game_library'),
-    path('/<slug:slug>', views.GameDetailView.as_view(), name='api_game_detail'),
-    path('/<slug:slug>/installers', views.GameInstallersView.as_view(), name='api_game_installers')
+    path('stats', views.GameStatsView.as_view(), name='api_game_stats'),
+    path('library/<username>', views.GameLibraryView.as_view(), name='api_game_library'),
+    path('<slug:slug>', views.GameDetailView.as_view(), name='api_game_detail'),
+    path('<slug:slug>/installers', views.GameInstallersView.as_view(), name='api_game_installers')
 ]
