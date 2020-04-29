cee0d03383f43d7676c0cccea46bf99236c6020e
Mathieu Comandon <strycore@gmail.com>
2020-04-29 03:38:16 -0700

    Remove related fields from games API view

--- lutrisweb/settings/base.py
+++ lutrisweb/settings/base.py
@@ -253,7 +253,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 250,
+    'PAGE_SIZE': 100,
 }
 
 # Shell Plus
