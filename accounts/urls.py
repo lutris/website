f2787200ab481ab690ed09d1862bb776eb8ea765
Mathieu Comandon <strycore@gmail.com>
2020-04-30 09:19:10 -0700

    Fix formatting

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,42 +10,42 @@ urlpatterns = [
              template_name='accounts/login.html',
              authentication_form=forms.LoginForm
          ),
-        name='login'),
+         name='login'),
     path('logout/',
          auth_views.LogoutView.as_view(
              template_name='accounts/logout.html'
          ),
-        name='logout'),
+         name='logout'),
     path('password/change/',
-        auth_views.PasswordChangeView.as_view(
-            template_name='accounts/password_change.html'
-        ),
-        name='password_change'),
+         auth_views.PasswordChangeView.as_view(
+             template_name='accounts/password_change.html'
+         ),
+         name='password_change'),
     path('password/change/done/',
          auth_views.PasswordChangeDoneView.as_view(
              template_name='accounts/password_change_done.html'
          ),
-        name='password_change_done'),
+         name='password_change_done'),
     path('password/reset/',
          auth_views.PasswordResetView.as_view(
              template_name='accounts/password_reset.html'
          ),
-        name='password_reset'),
+         name='password_reset'),
     re_path(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',  # noqa
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='accounts/password_reset_confirm.html'
-        ),
-        name='password_reset_confirm'),
+            auth_views.PasswordResetConfirmView.as_view(
+                template_name='accounts/password_reset_confirm.html'
+            ),
+            name='password_reset_confirm'),
     path('password/reset/done/',
          auth_views.PasswordResetDoneView.as_view(
              template_name='accounts/password_reset_done.html'
          ),
-        name='password_reset_done'),
+         name='password_reset_done'),
     path('password/reset/complete/',
          auth_views.PasswordResetCompleteView.as_view(
              template_name='accounts/password_reset_complete.html'
          ),
-        name='password_reset_complete'),
+         name='password_reset_complete'),
 
     path('register/', views.register, name="register"),
     path('clear-auth-token/', views.clear_auth_token, name="clear_auth_token"),
