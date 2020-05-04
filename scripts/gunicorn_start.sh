c10b2f39192070ce458986dff49eaf3e77959512
Mathieu Comandon <strycore@gmail.com>
2020-05-04 16:05:02 -0700

    Make gunicorn script runnable in Docker

--- scripts/gunicorn_start.sh
+++ scripts/gunicorn_start.sh
@@ -2,26 +2,19 @@
 
 NAME="lutris"
 PORT=8080
-DJANGODIR=/srv/lutris/lutris.net
-SOCKFILE=/srv/lutris/run/gunicorn.sock
+DJANGODIR=/app
 USER=django
 GROUP=django
 NUM_WORKERS=9
-DJANGO_SETTINGS_MODULE=lutrisweb.settings.production
 DJANGO_WSGI_MODULE=lutrisweb.wsgi
 
 echo "Starting $NAME as `whoami`"
 
 cd $DJANGODIR
-source ../bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
-source ../bin/envvars
 
-RUNDIR=$(dirname $SOCKFILE)
-test -d $RUNDIR || mkdir -p $RUNDIR
-
-exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
     --name $NAME \
     --workers $NUM_WORKERS \
     --user=$USER --group=$GROUP \
