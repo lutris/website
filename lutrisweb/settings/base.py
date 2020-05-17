4d1bdf9f2547a4cd6eb90b2f2faa23e4d9498142
Mathieu Comandon <strycore@gmail.com>
2020-05-17 13:45:55 -0700

    Increase throttle rate and default page size, make Rest Framework consistent accross environments

--- lutrisweb/settings/base.py
+++ lutrisweb/settings/base.py
@@ -251,12 +251,16 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
     ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '60/min',
+        'user': '60/min'
+    },
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 250,
 }
 
 # Shell Plus
