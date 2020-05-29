b9d8a54c12badaddf4018f4c1e8b31b04ee8e3e2
Mathieu Comandon <strycore@gmail.com>
2020-05-29 12:50:39 -0700

    Add task to load games from GOGDB

--- providers/management/commands/load_gogdb.py
+++ providers/management/commands/load_gogdb.py
@@ -1,4 +1,4 @@
-"""Load a list of games from MAME"""
+"""Load a list of games from a GOGDB export"""
 import json
 from django.core.management.base import BaseCommand
 from providers import models
@@ -6,21 +6,25 @@ from providers import models
 
 class Command(BaseCommand):
 
+    provider_id = "GOGDB"
+
     def add_arguments(self, parser):
         parser.add_argument(
             "file_path",
             nargs=1,
-            help="Path to the MAME game list"
+            help="Path to the GOG DB game list"
         )
 
     def handle(self, *args, **options):
         file_path = options["file_path"][0]
-        provider, _created = models.Provider.objects.get_or_create(name="MAME")
+        provider, _created = models.Provider.objects.get_or_create(name=self.provider_id)
         with open(file_path) as list_file:
             game_list = json.load(list_file)
         for game in game_list:
+            if "product_id" not in game:
+                continue
             provider_game, _created = models.ProviderGame.objects.get_or_create(
-                slug=game["mameid"],
+                slug=game["product_id"],
                 provider=provider
             )
             provider_game.name = game["name"]
@@ -28,7 +32,7 @@ class Command(BaseCommand):
             provider_game.save()
             print("%s - %s %s (%s)" % (
                 game["name"],
-                game["year"],
-                game["publisher"],
-                game["mameid"]
+                game.get("release_date"),
+                game.get("publisher"),
+                game["product_id"]
             ))
