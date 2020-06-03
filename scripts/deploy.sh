4e1ea002ef9a625bf735c5d3fbb2ebc8538d62eb
Mathieu Comandon <strycore@gmail.com>
2020-06-03 00:04:12 -0700

    Add Docker build stage for Sphinx docs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -e
 export DEPLOY_ENV=$1
 export DEPLOY_HOST=$2
 
-export DOCKER_HOST="ssh://$DEPLOY_HOST"
+# export DOCKER_HOST="ssh://$DEPLOY_HOST"
 export COMPOSE_PROJECT_NAME=lutrisweb_$DEPLOY_ENV
 if [[ $DEPLOY_ENV == "prod" ]]; then
     export POSTGRES_HOST_PORT=5435
@@ -15,7 +15,7 @@ else
     export HTTP_PORT=81
 fi
 
-docker-compose -f docker-compose.prod.yml build lutrisweb
-docker-compose -f docker-compose.prod.yml build lutrisworker
-docker-compose -f docker-compose.prod.yml up -d
-docker-compose -f docker-compose.prod.yml restart lutrisnginx
+docker-compose --verbose -f docker-compose.prod.yml build lutrisweb
+docker-compose --verbose -f docker-compose.prod.yml build lutrisworker
+docker-compose --verbose -f docker-compose.prod.yml up -d
+docker-compose --verbose -f docker-compose.prod.yml restart lutrisnginx
