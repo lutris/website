e497b60d996859da6aeaa5e581cf3d387317f645
Mathieu Comandon <strycore@gmail.com>
2020-06-03 02:10:34 -0700

    Build frontend files in a different stage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,9 @@ set -e
 export DEPLOY_ENV=$1
 export DEPLOY_HOST=$2
 
-# export DOCKER_HOST="ssh://$DEPLOY_HOST"
+if [[ $DEPLOY_HOST ]]; then
+    export DOCKER_HOST="ssh://$DEPLOY_HOST"
+fi
 export COMPOSE_PROJECT_NAME=lutrisweb_$DEPLOY_ENV
 if [[ $DEPLOY_ENV == "prod" ]]; then
     export POSTGRES_HOST_PORT=5435
@@ -15,7 +17,7 @@ else
     export HTTP_PORT=81
 fi
 
-docker-compose --verbose -f docker-compose.prod.yml build lutrisweb
-docker-compose --verbose -f docker-compose.prod.yml build lutrisworker
-docker-compose --verbose -f docker-compose.prod.yml up -d
-docker-compose --verbose -f docker-compose.prod.yml restart lutrisnginx
+docker-compose -f docker-compose.prod.yml build lutrisweb
+docker-compose -f docker-compose.prod.yml build lutrisworker
+docker-compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.prod.yml restart lutrisnginx
