84a09df74c8c41016e06d10dd83e3d7db10f34e4
Mathieu Comandon <strycore@gmail.com>
2020-06-20 16:46:39 -0700

    Output more information on the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,7 @@ export DEPLOY_HOST=$2
 
 if [[ $DEPLOY_HOST ]]; then
     export DOCKER_HOST="ssh://$DEPLOY_HOST"
+    echo "DOCKER_HOST set to $DOCKER_HOST"
 fi
 export COMPOSE_PROJECT_NAME=lutrisweb_$DEPLOY_ENV
 if [[ $DEPLOY_ENV == "prod" ]]; then
@@ -17,7 +18,11 @@ else
     export HTTP_PORT=81
 fi
 
+echo "Building lutrisweb"
 docker-compose -f docker-compose.prod.yml build lutrisweb
+echo "Building lutrisworker"
 docker-compose -f docker-compose.prod.yml build lutrisworker
+echo "Bringing Docker Compose up"
 docker-compose -f docker-compose.prod.yml up -d
+echo "Restarting NGinx"
 docker-compose -f docker-compose.prod.yml restart lutrisnginx
