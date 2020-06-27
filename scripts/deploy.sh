dcc7494530a9657eb213487214cddb9a60a7e530
Mathieu Comandon <strycore@gmail.com>
2020-06-27 14:51:47 -0700

    Set Docker Compose options at deploy time

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,12 @@ set -e
 export DEPLOY_ENV=$1
 export DEPLOY_HOST=$2
 
+COMPOSE_OPTS="--compress --parallel"
+if [[ "$3" == "--no-cache" ]]; then
+    # Add --no-cache to build to disable cache and rebuild documentation.
+    COMPOSE_OPTS="$COMPOSE_OPTS --no-cache"
+fi
+
 if [[ $DEPLOY_HOST ]]; then
     export DOCKER_HOST="ssh://$DEPLOY_HOST"
     echo "DOCKER_HOST set to $DOCKER_HOST"
@@ -18,10 +24,10 @@ else
     export HTTP_PORT=81
 fi
 
-echo "Building lutrisweb"
-docker-compose -f docker-compose.prod.yml build lutrisweb
-echo "Building lutrisworker"
-docker-compose -f docker-compose.prod.yml build lutrisworker
+docker-compose -f docker-compose.prod.yml build $COMPOSE_OPTS lutrisweb
+
+docker-compose -f docker-compose.prod.yml build $COMPOSE_OPTS lutrisworker
+
 echo "Bringing Docker Compose up"
 docker-compose -f docker-compose.prod.yml up -d
 echo "Restarting NGinx"
