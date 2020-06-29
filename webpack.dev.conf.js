7982f89613712794f6d793378a1bacb5f5555020
Mathieu Comandon <strycore@gmail.com>
2020-06-29 13:20:13 -0700

    Reformat webpack conf

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,95 +1,91 @@
-'use strict'
+"use strict";
 const webpack = require("webpack");
-const autoprefixer = require('autoprefixer');
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require("autoprefixer");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: [
-            "./frontend/js/app.js",
-            "./frontend/css/lutris.scss",
+  entry: ["./frontend/js/app.js", "./frontend/css/lutris.scss"],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "js/app.js",
+    // path to build relative asset links
+    publicPath: "../",
+  },
+  devtool: "inline-source-map",
+  mode: "development",
+  module: {
+    rules: [
+      // expose jQuery to other scripts
+      {
+        test: require.resolve("jquery"),
+        use: [
+          {
+            loader: "expose-loader",
+            options: "jQuery",
+          },
+          {
+            loader: "expose-loader",
+            options: "$",
+          },
         ],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: "js/app.js",
-        // path to build relative asset links
-        publicPath: "../"
-    },
-    devtool: "inline-source-map",
-    mode: "development",
-    module: {
-        rules: [
-            // expose jQuery to other scripts
-            {
-                test: require.resolve('jquery'),
-                use: [{
-                    loader: 'expose-loader',
-                    options: 'jQuery'
-                },{
-                    loader: 'expose-loader',
-                    options: '$'
-                }]
-            },
-            {
-                test: /\.(jpg|png|svg|bmp|gif)$/,
-                use: {
-                  loader: 'url-loader',
-                },
-            },
-            // styles loader
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ],
-            },
+      },
+      {
+        test: /\.(jpg|png|svg|bmp|gif)$/,
+        use: {
+          loader: "url-loader",
+        },
+      },
+      // styles loader
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
 
-            // fonts loader
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "fonts/[name].[ext]"
-                        }
-                    },
-                ],
+      // fonts loader
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[ext]",
             },
-        ]
-    },
-    plugins: [
-        // save compiled SCSS into separated CSS file
-        new MiniCssExtractPlugin({
-            filename: "css/lutris.css"
-        }),
-        // provide jQuery and Popper.js dependencies
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            jquery: 'jquery',
-            'window.jQuery': 'jquery',
-            'window.$': 'jquery',
-            Popper: ['popper.js', 'default'],
-        }),
-        // new CopyPlugin({
-        //     patterns: [
-        //         { from: path.resolve(__dirname, 'node_modules/ace-builds/src-noconflict/*'),
-        //             to: path.resolve(__dirname, 'public/ace'),
-        //             flatten: true },
-        //         { from: path.resolve(__dirname, 'node_modules/ace-builds/src-noconflict/snippets/*'),
-        //             to: path.resolve(__dirname, 'public/ace/snippets'),
-        //             flatten: true },
-        //     ]
-        // }),
+          },
+        ],
+      },
     ],
-    // watch: true,
-    // watchOptions: {
-    //     ignored: /node_modules/,
-    //     aggregateTimeout: 300,
-    //     poll: 500
-    //   },
+  },
+  plugins: [
+    // save compiled SCSS into separated CSS file
+    new MiniCssExtractPlugin({
+      filename: "css/lutris.css",
+    }),
+    // provide jQuery and Popper.js dependencies
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      jquery: "jquery",
+      "window.jQuery": "jquery",
+      "window.$": "jquery",
+      Popper: ["popper.js", "default"],
+    }),
+    // new CopyPlugin({
+    //     patterns: [
+    //         { from: path.resolve(__dirname, 'node_modules/ace-builds/src-noconflict/*'),
+    //             to: path.resolve(__dirname, 'public/ace'),
+    //             flatten: true },
+    //         { from: path.resolve(__dirname, 'node_modules/ace-builds/src-noconflict/snippets/*'),
+    //             to: path.resolve(__dirname, 'public/ace/snippets'),
+    //             flatten: true },
+    //     ]
+    // }),
+  ],
+  // watch: true,
+  // watchOptions: {
+  //     ignored: /node_modules/,
+  //     aggregateTimeout: 300,
+  //     poll: 500
+  //   },
 };
