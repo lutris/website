554327fe0260bf244c93a76c8300d3011bc6090e
Mathieu Comandon <strycore@gmail.com>
2021-11-28 16:57:35 -0800

    Add collectstatic to deploy script

--- scripts/staging-deploy.sh
+++ scripts/staging-deploy.sh
@@ -2,10 +2,21 @@
 
 set -e
 
-cd /srv/staging/website
+ROOT_DIR=/srv/staging
+DJANGODIR=$ROOT_DIR/website/
+
+source $ROOT_DIR/venv/bin/activate
+export $(cat .env.staging | xargs)
+
+cd $DJANGODIR
 git pull
+
 npm run build
 npm run build-prod
+
+./manage.py collectstatic --clear --noinput \
+    --ignore less/test/* --ignore select2/docs/*
+
 sudo systemctl stop gunicorn_staging
 sleep 5
 sudo systemctl start gunicorn_staging
