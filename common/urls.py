a4a03d222217d1e0b50224f39e66d747a77174c2
Mathieu Comandon <strycore@gmail.com>
2021-12-02 16:13:14 -0800

    Restyle buttons + fix homepage route

--- common/urls.py
+++ common/urls.py
@@ -5,10 +5,10 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    re_path('/?', views.home, name="homepage"),
+    re_path('^/?$', views.home, name="homepage"),
     re_path('about/?', TemplateView.as_view(template_name='common/about.html'), name='about'),
     re_path('downloads/?', views.Downloads.as_view(), name='downloads'),
     re_path('upload/?', views.upload_file, name='upload_file'),
     re_path('faq/?', TemplateView.as_view(template_name='common/faq.html'), name='faq'),
-    re_path('donate/?', TemplateView.as_view(template_name='common/donate.html'), name='donate')
+    re_path('donate/?', TemplateView.as_view(template_name='common/donate.html'), name='donate'),
 ]
