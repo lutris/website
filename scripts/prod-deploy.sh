1afa74f18a338f187848ce0bb6ae141ddbb997a8
Mathieu Comandon <strycore@gmail.com>
2021-12-07 17:18:48 -0800

    Add script for prod deploys

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-ROOT_DIR=/srv/staging
+ROOT_DIR=/srv/prod
 DJANGODIR=$ROOT_DIR/website/
 
 cd $DJANGODIR
 git pull
 
 source $ROOT_DIR/venv/bin/activate
-export $(cat .env.staging | xargs)
+export $(cat .env.prod | xargs)
 
 if [[ "$1" == "--webpack" ]]; then
     npm run build
@@ -22,4 +22,4 @@ fi
 ./manage.py collectstatic --clear --noinput \
     --ignore less/test/* --ignore select2/docs/*
 
-sudo systemctl restart gunicorn_staging
+sudo systemctl restart gunicorn_prod
