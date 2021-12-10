0c48cf56721c53c4e1c2402614987b3726270046
Mathieu Comandon <strycore@gmail.com>
2021-12-09 16:53:58 -0800

    Fix common URLs

--- common/urls.py
+++ common/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 urlpatterns = [
     re_path('^/?$', views.home, name="homepage"),
-    re_path('about/?', TemplateView.as_view(template_name='common/about.html'), name='about'),
-    re_path('downloads/?', views.Downloads.as_view(), name='downloads'),
-    re_path('upload/?', views.upload_file, name='upload_file'),
-    re_path('faq/?', TemplateView.as_view(template_name='common/faq.html'), name='faq'),
-    re_path('donate/?', TemplateView.as_view(template_name='common/donate.html'), name='donate'),
+    re_path('^about/?$', TemplateView.as_view(template_name='common/about.html'), name='about'),
+    re_path('^downloads/?$', views.Downloads.as_view(), name='downloads'),
+    re_path('^upload/?$', views.upload_file, name='upload_file'),
+    re_path('^faq/?$', TemplateView.as_view(template_name='common/faq.html'), name='faq'),
+    re_path('^donate/?$', TemplateView.as_view(template_name='common/donate.html'), name='donate'),
 ]
