b006e37a2f3455cc0a2ee04a49ac2c1497b88d0a
Mathieu Comandon <strycore@gmail.com>
2021-12-15 18:25:07 -0800

    Fix typo

--- games/views/pages.py
+++ games/views/pages.py
@@ -232,7 +232,7 @@ def game_detail(request, slug):
     installers = game.installers.published()
 
     unpublished_installers = game.installers.unpublished()
-    if not user.is_staff():
+    if not user.is_staff:
         unpublished_installers = unpublished_installers.filter(user=user)
 
     pending_change_subm_count = 0
