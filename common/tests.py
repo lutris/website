11880b588ca2b718447017c5f553bbba6a9dfe4a
Mathieu Comandon <strycore@gmail.com>
2021-12-15 16:42:57 -0800

    Remove news feed test

--- common/tests.py
+++ common/tests.py
@@ -13,13 +13,6 @@ class PagesTest(TestCase):
         response = self.client.get("/foobar/baz")
         self.assertEqual(response.status_code, 404)
 
-
-class TestNewsFeed(TestCase):
-    def test_feed_availability(self):
-        response = self.client.get("/news/feed/")
-        self.assertEqual(response.status_code, 200)
-
-
 class TestUtils(TestCase):
     def test_slugify(self):
         self.assertEqual(slugify(None), "")
