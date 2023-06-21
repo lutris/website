101ed99ff461c3a0dfa2ac41893c85529c32650c
Mathieu Comandon <strycore@gmail.com>
2023-06-21 12:45:48 -0700

    Add min_version on runtimes

--- runners/models.py
+++ runners/models.py
@@ -84,7 +84,8 @@ class Runtime(models.Model):
     architecture = models.CharField(max_length=8, choices=ARCH_CHOICES, default="all")
     url = models.URLField(blank=True)
     enabled = models.BooleanField(default=True)
-    versioned = models.BooleanField(default=False)
+    versioned = models.BooleanField(default=False)  # Store each version in its own folder
+    min_version = models.CharField(max_length=8, blank=True)  # Restrict runtimes to client above given version
 
     class Meta:
         ordering = ("-created_at",)
