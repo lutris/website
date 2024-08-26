8139029b45be0a520f48eae27959fe5c0bd7cd12
Mathieu Comandon <strycore@gmail.com>
2024-08-25 22:10:17 -0700

    Add umu Celery task module

--- providers/tasks/__init__.py
+++ providers/tasks/__init__.py
@@ -1,7 +1,32 @@
 """Global provider task module, expose Celery tasks here."""
+
 from providers.tasks.gog import load_gog_games, match_gog_games
-from providers.tasks.igdb import (deduplicate_igdb_games, load_igdb_covers,
-                                  load_igdb_games, load_igdb_genres,
-                                  load_igdb_platforms, match_igdb_games,
-                                  sync_igdb_coverart, sync_igdb_platforms)
+from providers.tasks.igdb import (
+    deduplicate_igdb_games,
+    load_igdb_covers,
+    load_igdb_games,
+    load_igdb_genres,
+    load_igdb_platforms,
+    match_igdb_games,
+    sync_igdb_coverart,
+    sync_igdb_platforms,
+)
 from providers.tasks.steam import load_steam_games, match_steam_games
+from providers.tasks.umu import update_umu_games
+
+
+__all__ = [
+    "load_gog_games",
+    "match_gog_games",
+    "load_igdb_covers",
+    "load_igdb_genres",
+    "load_igdb_platforms",
+    "load_igdb_games",
+    "deduplicate_igdb_games",
+    "match_igdb_games",
+    "sync_igdb_coverart",
+    "sync_igdb_platforms",
+    "load_steam_games",
+    "match_steam_games",
+    "update_umu_games",
+]
---
+++ providers/tasks/umu.py
@@ -0,0 +1,19 @@
+"""Compare GOG games to the Lutris library"""
+
+from celery.utils.log import get_task_logger
+
+from providers.umu import update_repository, import_umu_games
+from common.models import save_action_log
+from lutrisweb.celery import app
+from games.webhooks import send_simple_message
+
+LOGGER = get_task_logger(__name__)
+
+
+@app.task
+def update_umu_games():
+    """Task to load GOG games from the API"""
+    update_repository()
+    stats = import_umu_games()
+    save_action_log("update_protonfixes", stats)
+    send_simple_message("umu games loaded: %s" % stats)
